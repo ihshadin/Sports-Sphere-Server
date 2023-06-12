30f17a7ffe1a48ad79c3c3443a12c255c2d13146
IH Shadin <shadinfr@yahoo.com>
2023-06-12 19:38:05 +0600

    final commit

--- index.js
+++ index.js
@@ -187,7 +187,7 @@ async function run() {
             const result = await userCollection.insertOne(user);
             res.send(result)
         })
-        app.get('/users/role/:email', verifyJWT, async (req, res) => {
+        app.get('/users/role/:email', async (req, res) => {
             const email = req.params.email;
             const query = { email: email };
             const result = await userCollection.findOne(query);
